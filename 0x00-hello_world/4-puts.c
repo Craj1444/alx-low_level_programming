ba6917230adc8148018cb526582e7e669bf23e6f
Craj 1444 <crajmohamud@gmail.com>
2023-06-15 14:07:51 -0700

    Hello C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include<string.h> 
+  
+ /** 
+  * main - Entry point 
+  * 
+  * Declaration: 'The program's description' 
+  * Return: Always 0 (success) 
+  */ 
+  
+ int main(void) 
+ { 
+         char str1[70]; 
+  
+         strcpy(str1, "\"Programming is like building a multilingual puzzle"); 
+  
+         puts(str1); 
+         return (0); 
+ }
